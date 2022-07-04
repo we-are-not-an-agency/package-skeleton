b3dcacf3571a79233faea95d83430f35f08933ca
Diana Pukaine <diana.pukaine@cube.lv>
2022-07-04 11:37:48 +0300

    chore: add filament support

--- src/MyPackageServiceProvider.php
+++ src/MyPackageServiceProvider.php
@@ -2,81 +2,18 @@
 
 namespace WANAA\:uc:package;
 
+use Filament\PluginServiceProvider;
 use Illuminate\Support\ServiceProvider;
 
-class :uc:packageServiceProvider extends ServiceProvider
+class :uc:packageServiceProvider extends PluginServiceProvider
 {
-    /**
-     * Perform post-registration booting of services.
-     *
-     * @return void
-     */
-    public function boot(): void
+    public function configurePackage(Package $package): void
     {
-        // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'wanaa');
-        // $this->loadViewsFrom(__DIR__.'/../resources/views', 'wanaa');
-        // $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-        // $this->loadRoutesFrom(__DIR__.'/routes.php');
-
-        // Publishing is only necessary when using the CLI.
-        if ($this->app->runningInConsole()) {
-            $this->bootForConsole();
-        }
-    }
-
-    /**
-     * Register any package services.
-     *
-     * @return void
-     */
-    public function register(): void
-    {
-        $this->mergeConfigFrom(__DIR__.'/../config/:lc:package.php', ':lc:package');
-
-        // Register the service the package provides.
-        $this->app->singleton(':lc:package', function ($app) {
-            return new :uc:package;
-        });
-    }
-
-    /**
-     * Get the services provided by the provider.
-     *
-     * @return array
-     */
-    public function provides()
-    {
-        return [':lc:package'];
-    }
-
-    /**
-     * Console-specific booting.
-     *
-     * @return void
-     */
-    protected function bootForConsole(): void
-    {
-        // Publishing the configuration file.
-        $this->publishes([
-            __DIR__.'/../config/:lc:package.php' => config_path(':lc:package.php'),
-        ], ':lc:package.config');
-
-        // Publishing the views.
-        /*$this->publishes([
-            __DIR__.'/../resources/views' => base_path('resources/views/vendor/wanaa'),
-        ], ':lc:package.views');*/
-
-        // Publishing assets.
-        /*$this->publishes([
-            __DIR__.'/../resources/assets' => public_path('vendor/wanaa'),
-        ], ':lc:package.views');*/
-
-        // Publishing the translation files.
-        /*$this->publishes([
-            __DIR__.'/../resources/lang' => resource_path('lang/vendor/wanaa'),
-        ], ':lc:package.views');*/
-
-        // Registering package commands.
-        // $this->commands([]);
+        $package->name(':lc:package');
+            // ->hasConfigFile();
+            // ->hasTranslations();
+            // ->hasCommands();
+            // ->hasAssets();
+            // ->hasViews();
     }
 }
